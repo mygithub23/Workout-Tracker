c97d368e475d30c7ca76454e7d10e1d136b0d103
Ali Alaoui <madkar777@gmail.com>
2021-03-20 12:25:15 -0400

    update

--- routes/html.js
+++ routes/html.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = function(app) {
 
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "index.html"));
+        res.sendFile(path.join(__dirname, "..", "index.html"));
     })
     app.get("/stats", function(req, res) {
         res.sendFile(path.join(__dirname, "..", "public", "stats.html"));
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require("express");
+const path = require('path');
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
+//once deployed to heroku remove this line.
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/workout";
+
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", {
+mongoose.connect(dbUrl,{ 
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,20 +37,20 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout", { 
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
+
+
+//mongodb+srv://mytracker:<password>@cluster0.6pgpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
 
 require("./routes/api.js")(app);
 require("./routes/html.js")(app);
 
 
 
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+const port = process.env.PORT || 32000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
+})
